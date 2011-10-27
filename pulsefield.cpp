29c586ed2a523931b924f2b13ee705256e956ab2
U-edgee\tomas <tomas@edgee.(none)>
2011-10-27 20:13:33 +0200

    Added file pulsefield.h and pulsefield.cpp

---
+++ pulsefield.cpp
@@ -0,0 +1,11 @@
+#include <irrlicht.h>
+#include "driverChoice.h"
+#include <iostream>
+#include <fstream>
+#include <matrix4.h>
+#include "field.h"
+#include "pulsefield.h"
+
+using namespace irr;
+using namespace std;
+using core::vector3df;
