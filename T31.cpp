9d8d2fdecf9418da2935ecb69ba9c27710a4b456
Elvar Örn Unnþórsson <ellioman@gmail.com>
2011-11-07 20:21:24 +0100

    Small texture test with the pulse

--- T31.cpp
+++ T31.cpp
@@ -13,48 +13,143 @@
 //////////////////////////////
 T31::T31(ISceneManager* smgr, IVideoDriver* driver, int x, int y, playground pg) : Floor(smgr,driver,x,y,pg)
 {
-	texture=driver->getTexture(texturepath+"particlewhite.bmp");	
-	block->setMaterialTexture(0,texture); 
 	this->isActive = true;
 	this->timePassed = 0.0;
+	this->texIndexIncrement = 2;
+	this->texIndex = 0;
+	
+	// Texture stuff
+	block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor0.bmp") );
 };
 
-void T31::introduceTo(Field &f) {
+
+void T31::introduceTo(Field &f)
+{
 	// Should add this field to friendFields vector.
-	int place = sizeof(this->friendFields)/sizeof(Field) -1;
+	int place = sizeof(this->friendFields)/sizeof(Field) - 1;
 	this->friendFields[place] = &f;
-	
 }
 
 
-void T31::sphereOverlap(Sphere &s, f32 xoverlap, f32 yoverlap) {
+void T31::sphereOverlap(Sphere &s, f32 xoverlap, f32 yoverlap)
+{
 	// Might not need this. 
 	// We should call this function in other fields.
 	Floor::sphereOverlap(s, xoverlap, yoverlap);
 }
 
-void T31::handleSphere(Sphere &s, position2di mousemove, f32 frameDeltaTime) {
+
+void T31::handleSphere(Sphere &s, position2di mousemove, f32 frameDeltaTime)
+{
 	Floor::handleSphere(s, mousemove, frameDeltaTime);
+	
 	// Need to keep on counting.
 	timeProgress(frameDeltaTime);
-	if (isActive) {
+	if (isActive)
+	{
 		vector3df here = vector3df(getx()*fieldsize, 0, gety()*fieldsize);
-		vector3df zero = vector3df(0,0,0);
-		s.setPosition(here);
-		s.setVelocity(zero);
+		vector3df zero = here + s.getVelocity();
+		//s.setPosition(here);
+		//s.setVelocity(zero);
+		
+		vector3df p = s.getPosition();
+		vector3df v = s.getVelocity();
+		s.setPosition(p);
+		s.setVelocity(v);
+		
+		
 	}
+	this->changeTexture();
 }
 
-void T31::timeProgress(f32 frameDeltaTime) {
-	if (timePassed > timeActive) {
+void T31::changeTexture()
+{
+	if ( this->texIndex < 100 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor0.bmp") ); 
+	}
+	else if ( this->texIndex < 200 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor1.bmp") ); 
+	}
+	else if ( this->texIndex < 250 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor2.bmp") ); 
+	}
+	else if ( this->texIndex < 300 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor3.bmp") ); 
+	}
+	else if ( this->texIndex < 350 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor4.bmp") ); 
+	}
+	else if ( this->texIndex < 400 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor5.bmp") ); 
+	}
+	else if ( this->texIndex < 450 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor6.bmp") ); 
+	}
+	else if ( this->texIndex < 500 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor7.bmp") ); 
+	}
+	else if ( this->texIndex < 550 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor8.bmp") ); 
+	}
+	else if ( this->texIndex < 600 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor9.bmp") ); 
+	}
+	else if ( this->texIndex < 650 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor10.bmp") ); 
+	}
+	else if ( this->texIndex < 700 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor11.bmp") ); 
+	}
+	else if ( this->texIndex < 750 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor12.bmp") ); 
+	}
+	else if ( this->texIndex < 800 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor13.bmp") ); 
+	}
+	else if ( this->texIndex < 850 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor14.bmp") ); 
+	}
+	else if ( this->texIndex < 900 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor15.bmp") ); 
+	}
+	else if ( this->texIndex < 950 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor16.bmp") ); 
+	}
+	else if ( this->texIndex < 1000 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor17.bmp") ); 
+	}
+	else if ( this->texIndex < 1050 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor18.bmp") ); 
+	}
+	else if ( this->texIndex < 1100 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor19.bmp") ); 
+	}
+	else if ( this->texIndex < 1150 ) {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor20.bmp") ); 
+	}
+	else {
+		block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor0.bmp") ); 
+		this->texIndex = 0;
+	}
+	
+	this->texIndex += this->texIndexIncrement;
+}
+
+void T31::timeProgress(f32 frameDeltaTime)
+{
+	if (timePassed > timeActive)
+	{
 		// Time to switch the pulse off.
 		isActive = !isActive;
 		timePassed = 0.0;
-	} else {
+	} 
+	else
+	{
 		timePassed += frameDeltaTime;
 	}
 	
-	if (isActive) {
+	if (isActive)
+	{
 		// Need to check every friend field
 		// if ball is in that field.
 		// If we are active, then we apply force.
@@ -62,6 +157,20 @@ void T31::timeProgress(f32 frameDeltaTime) {
 	// else we do nothing
 }
 
-fieldtype T31::getFieldType(){ 
-  return tT31; 
+// when the sphere enters, change the texture to the alternate one
+void T31::sphereEnter(Sphere &s)
+{
+	//block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor9.bmp") );
+}
+
+// change back when the sphere leaves.
+void T31::sphereExit(Sphere &s)
+{
+	block->setMaterialTexture(0, driver->getTexture(texturepath + "pulsecolor0.bmp") );
+	this->texIndex = 0;
+}
+
+fieldtype T31::getFieldType()
+{ 
+	return tT31; 
 }
--- testfield.h
+++ testfield.h
@@ -11,48 +11,45 @@
 
 // Declaring one example of a Wall, called TestWall.
 
-class TestWall: public Wall{
-protected:
-  // To demonstrate the "introduceTo" feature, we have here a pointer to another
-  // field of type TestWall. (Explained in comments where it is used)
-  TestWall *related;
-public:
-  TestWall(ISceneManager* smgr,
-	IVideoDriver* driver,
-	     int x, int y, playground  pg) ;
-
-  // since the parent class Wall defines all the virtual methods of
-  // Field, we only need to declare those that we are going to change:
-  virtual fieldtype getFieldType();
-  virtual void sphereOverlap(Sphere &s, f32 xoverlap, f32 yoverlap);
-  virtual void introduceTo(Field &f);
+class TestWall: public Wall
+{
+	protected:
+		// To demonstrate the "introduceTo" feature, we have here a pointer to another
+		// field of type TestWall. (Explained in comments where it is used)
+		TestWall *related;
+		
+	public:
+		TestWall(ISceneManager* smgr, IVideoDriver* driver, int x, int y, playground  pg) ;
+		
+		// since the parent class Wall defines all the virtual methods of
+		// Field, we only need to declare those that we are going to change:
+		virtual fieldtype getFieldType();
+		virtual void sphereOverlap(Sphere &s, f32 xoverlap, f32 yoverlap);
+		virtual void introduceTo(Field &f);
 };
+
 
 // Declaring one example of a Floor, called LightFloor
-
-class LightFloor: public Floor{
-protected:
-  // this one contains a second texture (for when the sphere is on this floor)
-  ITexture *alt_texture;
-
-  // a counter for the time that the sphere was on this floor
-  f32 timeSpentHere;
-
-  // a flag whether the sphere is in free fall mode
-  bool falling;
-public:
-  LightFloor(ISceneManager* smgr,
-	IVideoDriver* driver,
-	     int x, int y, playground pg) ;
-
-  // again just declaring those methods that are going to change with respect to parent class:
-  virtual void sphereEnter(Sphere &s);
-  virtual void sphereExit(Sphere &s);
-  virtual void handleSphere(Sphere &s, position2di mousemove, f32 frameDeltaTime);
-  virtual fieldtype getFieldType();
+class LightFloor: public Floor
+{
+	protected:
+		// this one contains a second texture (for when the sphere is on this floor)
+		ITexture *alt_texture;
+		
+		// a counter for the time that the sphere was on this floor
+		f32 timeSpentHere;
+		
+		// a flag whether the sphere is in free fall mode
+		bool falling;
+	
+	public:
+		LightFloor(ISceneManager* smgr, IVideoDriver* driver, int x, int y, playground pg) ;
+		
+		// again just declaring those methods that are going to change with respect to parent class:
+		virtual void sphereEnter(Sphere &s);
+		virtual void sphereExit(Sphere &s);
+		virtual void handleSphere(Sphere &s, position2di mousemove, f32 frameDeltaTime);
+		virtual fieldtype getFieldType();
 };
-
-
-
 
 #endif
