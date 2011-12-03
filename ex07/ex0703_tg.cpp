9d48194c2988f9b792c58749f5b9df96d7f90255
tomgud <tomasgudm@gmail.com>
2011-12-03 13:39:44 +0100

    03 finished

--- ex07/ex0703_tg.cpp
+++ ex07/ex0703_tg.cpp
@@ -7,10 +7,11 @@ using namespace std;
 class vecNd {
 private:
 	double *elements;
-	int N; // Number of elemnts in this array
+	int N; // Number of elemntes allowed in this array
 public:
 	vecNd(int N = 1) {
 		elements = new double[N];
+		this->N = N;
 	}
 	
 	int get_N() {
@@ -18,41 +19,70 @@ public:
 	}
 	
 	double operator[] (int i) {
-		if (n < this-N) {
-			return elements[n];
+		if (i < this->N) {
+			return elements[i];
+		}
+	}
+	void set_elem_at(int i, double val) {
+		if (i < this->N) {
+			elements[i] = val;
 		}
 	}
 	
-	/* Not done yet
 	vecNd add(vecNd &v2) {
-		vecNd result(get_x() + v2[0], get_y() + v2[1]);
-		return result;
+		int larger = (this->N > v2.get_N()) ? this->N : v2.get_N();
+		vecNd result(larger);
+		for (int i = 0; i < larger; ++i) {
+			result.set_elem_at(i, 
+				((this->N > i) ? elements[i] : 0) +
+				((v2.get_N() > i ) ? v2[i] : 0));
+		}
+		 return result;
 	}
+	
 	void mul(double k) {
-		this->x *= k;
-		this->y *= k;
+		for (int i = 0; i < N; ++i) {
+			elements[i] *= k;
+		}
 	}
 	double product(vecNd &v2) {
-		return (get_x() * v2[0]) + (get_y() * v2[1]);
+		int smaller = (this->N < v2.get_N()) ? this->N : v2.get_N();
+		double result = 0.0;
+		for (int i = 0; i < smaller; ++i) {
+			result += elements[i] * v2[i];
+		}
+		return result;
+
 	}
 	
 	double length() {
 		return sqrt(product(*this));	
 	}
-	*/
+	
 };
 
 int main() {
-	vecNd v1(3,5);
-	vecNd v2(2,6);
-	vecNd v3 = v1.add(v2);
-	cout << "v1 [" << v1[0] << "," << v1[1] << "]" << endl;
-	cout << "v2 [" << v2[0] << "," << v2[1] << "]" << endl;
+	vecNd v1(2);
+	v1.set_elem_at(0, 3);
+	v1.set_elem_at(1, 6);
+	vecNd v2(3);
+	v2.set_elem_at(0, 2);
+	v2.set_elem_at(1, 5);
+	v2.set_elem_at(2, 7);
+	
+	vecNd v3 = v2.add(v1);
+	vecNd v4 = v1.add(v2);
 
-	cout << "v3: [" << v3[0] << "," << v3[1] << "]" << endl;
+	cout << "v1 [" << v1[0] << "," << v1[1] << "]" << endl;
+	cout << "v2 [" << v2[0] << "," << v2[1] << "," << v2[2] << "]" << endl;
+
+	cout << "v3: [" << v3[0] << "," << v3[1] << "," << v3[2] << "]" << endl;
+	cout << "v4: [" << v4[0] << "," << v4[1] << "," << v4[2] << "]" << endl;
+
 	v1.mul(4.5);
 	cout << "v1*(k=4.5): [" << v1[0] << "," << v1[1] << "]" << endl;
 	cout << "product(v1, v2) : " << v1.product(v2) << endl;
 	cout << "length(v1) : " << v1.length() << endl;
+	cout << "length(v3) : " << v3.length() << endl;
 	
 }
